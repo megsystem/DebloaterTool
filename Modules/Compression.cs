112e2a7c7d603a430b170cb4745984fd27f10e6e
Giovanni <39799614+megsystem@users.noreply.github.com>
2025-05-11 23:49:44 +0200

    optimized

--- Modules/Compression.cs
+++ Modules/Compression.cs
@@ -9,25 +9,8 @@ namespace DebloaterTool
         {
             string command = @"/c c:\windows\*.* /s /i /exe:lzx";
             Logger.Log($"Starting compression with arguments: {command}", Level.INFO);
-
-            try
-            {
-                ProcessStartInfo psi = new ProcessStartInfo
-                {
-                    FileName = "compact.exe",
-                    Arguments = command,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-
-                Process proc = Process.Start(psi);
-                proc.WaitForExit();
-                Logger.Log("Compression process completed.", Level.SUCCESS);
-            }
-            catch (Exception ex)
-            {
-                Logger.Log($"Exception occurred during compression: {ex.Message}", Level.CRITICAL);
-            }
+            HelperRunner.Command("compact.exe", command);
+            Logger.Log("Compression process completed.", Level.SUCCESS);
         }
     }
 }
