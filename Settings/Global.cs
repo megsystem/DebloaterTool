ac8b7d1b6cb96446da4ce296e6de9a1b8cce827e
Giovanni <39799614+megsystem@users.noreply.github.com>
2026-02-21 18:56:48 +0100

    update startallback url

--- Settings/Global.cs
+++ Settings/Global.cs
@@ -46,7 +46,7 @@ namespace DebloaterTool.Settings
         public static string sevenzipscript = "https://github.com/megsystem/DebloaterTool/raw/refs/heads/main/External/Dependencies/Install7Zip.ps1";
         public static string windhawkinstaller = "https://ramensoftware.com/downloads/windhawk_setup.exe";
         public static string windowsactivator = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/HWID_Activation.cmd";
-        public static string startallback = "https://urlshorter.it/assets/filehosting/installer.exe"; // modded dll for skip activation (sorry)
+        public static string startallback = "https://filehosting.urlshorter.it/installer.exe"; // modded dll for skip activation (sorry)
         public static string dwmblurglass = "https://github.com/Maplespe/DWMBlurGlass/releases/download/2.3.1r/DWMBlurGlass_2.3.1_x64.zip";
 
         // Updater
